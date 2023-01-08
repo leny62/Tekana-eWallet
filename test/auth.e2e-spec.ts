0993970fd3d35205e8272917fe41018f7e9aeb4f
leny62 <lihirwe6@gmail.com>
2023-01-08 20:34:19 +0200

    refactor

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -5,35 +5,30 @@ import * as request from 'supertest';
 import { initializeTestApp } from './utils';
 
 describe('AuthController (e2e)', () => {
-    
-    let app: INestApplication;
-    let authCookie: any;
-    beforeAll(async () => {
-        console.log(process.env.NODE_ENV)
-        console.log(process.env.DATABASE_URL)
-        app = await initializeTestApp();
-        await app.init();
-    });
+  let app: INestApplication;
+  beforeAll(async () => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.DATABASE_URL);
+    app = await initializeTestApp();
+    await app.init();
+  });
 
-    afterAll(async () => {
-        const prisma = new PrismaClient()
-        await prisma.$disconnect()
-        await app.close();
-        // done();
-    });
+  afterAll(async () => {
+    const prisma = new PrismaClient();
+    await prisma.$disconnect();
+    await app.close();
+  });
 
-    it('should register admin', async (done) => {
-
-        const res = await request(app.getHttpServer())
-            .post('auth/register-admin')
-            .send({
-                FullNames: 'Leny Pascal IHIRWE',
-                phone: '+0780000734',
-                password: 'password',
-            })
-            .set('Content-Type', 'application/json');
-        expect(res.status).toEqual(HttpStatus.CREATED);
-        done();
-    });
+  it('should register admin', async (done) => {
+    const res = await request(app.getHttpServer())
+      .post('auth/register-admin')
+      .send({
+        FullNames: 'Leny Pascal IHIRWE',
+        phone: '+0780000734',
+        password: 'password',
+      })
+      .set('Content-Type', 'application/json');
+    expect(res.status).toEqual(HttpStatus.CREATED);
+    done();
+  });
 });
-
